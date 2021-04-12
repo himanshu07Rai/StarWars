30f18a2f6618232e71b18d8d8fcb512f1a16243b
Himanshu Rai <raihimanshu22345@gmail.com>
2021-04-12 11:59:50 +0530

    Done

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,12 @@
-import Footer from "./Footer";
 import Header from "./Header";
 import Meta from "./Meta";
-import Nav from "./Nav";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Meta />
-      {/* <Image src="/id.png" width={128} height={110} /> */}
-      <Nav />
       <Header />
       {children}
-      <Footer />
     </>
   );
 };
--- pages/characters/[id].js
+++ pages/characters/[id].js
@@ -1,5 +1,70 @@
-const character = () => {
-  return <div>C</div>;
+import styled from "styled-components";
+import CharacterCard from "../../components/CharacterCard";
+
+// EXECUTED ON SERVER
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://akabab.github.io/starwars-api/api/id/${context.params.id}.json`
+  );
+  const character = await res.json();
+
+  return {
+    props: {
+      character,
+    },
+  };
 };
 
-export default character;
+export const getStaticPaths = async () => {
+  const res = await fetch("https://akabab.github.io/starwars-api/api/all.json");
+  const characters = await res.json();
+  const ids = characters.map((character) => character.id);
+
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+const Character = ({ character }) => {
+  return (
+    <CharacterContainer>
+      <StarfieldLeft />
+      <CharacterCard character={character} />
+      <StarfieldRight />
+    </CharacterContainer>
+  );
+};
+
+export default Character;
+
+const CharacterContainer = styled.div`
+  padding: 40px;
+  margin-top: 200px;
+`;
+
+const StarfieldLeft = styled.div`
+  position: fixed;
+  width: 100px;
+  top: 0;
+  background-repeat: repeat-y;
+  left: 0;
+  height: 1700px;
+  background-position: left center;
+  background-size: 100% auto;
+  background-image: url("https://static-mh.content.disney.io/starwars/assets/background/bg_starsL-fd4661a3ccea.jpg");
+`;
+
+const StarfieldRight = styled.div`
+  position: fixed;
+  width: 100px;
+  top: 0;
+  background-repeat: repeat-y;
+  right: 0;
+  height: 1700px;
+  background-position: right center;
+  background-size: auto 100%;
+  background-image: url("https://static-mh.content.disney.io/starwars/assets/background/bg_starsR-655c85e404d4.jpg");
+`;
